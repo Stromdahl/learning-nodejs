778351a7baf8f583aeb8ba8f9943c65f7d30267e
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2022-06-16 09:47:05 +0200

    Add partials

--- udemy-course/web-server/src/app.js
+++ udemy-course/web-server/src/app.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+
 
 // Setup static direcotry and views location
 app.use(express.static(publicDirectoryPath));
@@ -33,7 +38,9 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        helpText: 'This is some helpful text.'
+        helpText: 'This is some helpful text.',
+        name: 'Mattias Strömdahl'
+
     });
 });
 
