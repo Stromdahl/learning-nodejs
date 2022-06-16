a7fed5e4d84f446c23f359f0cc2c8326179c95ff
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2022-06-16 15:41:10 +0200

    added weather endpoint

--- udemy-course/web-server/src/app.js
+++ udemy-course/web-server/src/app.js
@@ -45,11 +45,31 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: "You must provide an address"
+        });
+    }
+
     res.send({
-        forecast: 25,
-        location: 'Svedala'
-    });
+        temperature: 25,
+        address: req.query.address
+    })
 });
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    }
+
+    console.log(req.query)
+
+    res.send({
+        products: []
+    })
+})
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
