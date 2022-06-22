50bb319702c8e7fb055e47ac5f1e34d00bc70fc6
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2022-06-22 20:32:41 +0200

    mongoose

--- udemy-course/task-manager/mongodb.js
+++ udemy-course/task-manager/mongodb.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 
-const connectionURL = 'mongodb://root:example@127.0.0.1:27017';
+const connectionURL = 'mongodb://root:password@127.0.0.1:27017';
 const databaseName = 'task-manager';
 
 const MongoClient = new mongodb.MongoClient(connectionURL);
@@ -12,7 +12,7 @@ MongoClient.connect((error, client) => {
     }
 
     const db = client.db(databaseName);
-
+    console.log("Connected");
     // db.collection('users').updateOne({
     //     _id: new mongodb.ObjectId('62ac76db6df04bb3635af1dd')
     // }, {
@@ -24,16 +24,4 @@ MongoClient.connect((error, client) => {
     // }).catch((error) => {
     //     console.log(error);
     // });
-
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
-    }).then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    });
 });
